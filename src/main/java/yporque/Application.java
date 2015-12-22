aef9e9cc0193f92e848929760ea8c190a02c81f5
unknown <fgiana@PC-FGIANA-01.ARRS.ARRISI.com>
2015-12-22 16:38:54 -0300

    username Administrador default added

--- src/main/java/yporque/Application.java
+++ src/main/java/yporque/Application.java
@@ -10,8 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import yporque.model.Customer;
 import yporque.model.Product;
+import yporque.model.Vendedor;
 import yporque.repository.CustomerRepository;
 import yporque.repository.ProductRepository;
+import yporque.repository.VendedorRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("yporque")
@@ -19,54 +23,20 @@ public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
 
-
-/*
 	@Bean
-	public CommandLineRunner loadData(CustomerRepository repository, ProductRepository productRepository) {
+	public CommandLineRunner loadData(VendedorRepository vendedorRepository) {
 		return (args) -> {
-
-			productRepository.save(new Product("10001","Remera Niños",10,100.0));
-			productRepository.save(new Product("10002","Remera Niñas",10,200.0));
-			productRepository.save(new Product("10003","Zapatitos Niños",10,300.0));
-			productRepository.save(new Product("10004","Zapatitos Niñas",10,600.0));
-
-
-			// save a couple of customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
+			List<Vendedor> list = vendedorRepository.findByUsername("Administrador");
+			if(list.size() == 0){
+				vendedorRepository.save(new Vendedor("Administrador","1qaz2wsx","Administrador","Administrador"));
 			}
-			log.info("");
 
-			// fetch an individual customer by ID
-			Customer customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastNameStartsWithIgnoreCase('Bauer'):");
-			log.info("--------------------------------------------");
-			for (Customer bauer : repository
-					.findByLastNameStartsWithIgnoreCase("Bauer")) {
-				log.info(bauer.toString());
-			}
-			log.info("");
 		};
 	}
-	*/
 
 }
