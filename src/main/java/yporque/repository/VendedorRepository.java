5e1bb35036848de98b57687198d8657882a77968
unknown <fgiana@PC-FGIANA-01.ARRS.ARRISI.com>
2015-12-23 18:39:08 -0300

    added controller and updated unit test for Vendedor

--- src/main/java/yporque/repository/VendedorRepository.java
+++ src/main/java/yporque/repository/VendedorRepository.java
@@ -1,5 +1,7 @@
 package yporque.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yporque.model.Vendedor;
 
@@ -10,4 +12,5 @@ import java.util.List;
  */
 public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
     List<Vendedor> findByUsername(String username);
+    Page<Vendedor> findByUsernameContainingIgnoreCaseOrNombreContainingIgnoreCaseOrApellidoContainingIgnoreCase(String username, String nombre, String apellido, Pageable pageable);
 }
--- src/test/java/yporque/repository/VendedorRepositoryTest.java
+++ src/test/java/yporque/repository/VendedorRepositoryTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import yporque.config.MemoryDBConfig;
@@ -50,6 +53,25 @@ public class VendedorRepositoryTest {
         List<Vendedor> list = vendedorRepository.findByUsername("username1");
         Assert.assertThat(list,hasSize(1));
         Assert.assertThat(list.get(0).toString(),is(v.toString()));
+    }
+
+    @Test
+    public void test_find_vendedor_by_username_or_nombre_or_apellido() throws Exception {
+        Vendedor v = new Vendedor("username1","password1","nombre1","apellido1");
+        vendedorRepository.save(v);
+        Page<Vendedor> list = vendedorRepository.findByUsernameContainingIgnoreCaseOrNombreContainingIgnoreCaseOrApellidoContainingIgnoreCase("Sername","Sername","Sername",new PageRequest(0,10));
+        Assert.assertThat(list.getContent(),hasSize(1));
+        Assert.assertThat(list.getContent().get(0).toString(),is(v.toString()));
+
+        list = vendedorRepository.findByUsernameContainingIgnoreCaseOrNombreContainingIgnoreCaseOrApellidoContainingIgnoreCase("OMB","OMB","OMB",new PageRequest(0,10));
+        Assert.assertThat(list.getContent(),hasSize(1));
+        Assert.assertThat(list.getContent().get(0).toString(),is(v.toString()));
+
+        list = vendedorRepository.findByUsernameContainingIgnoreCaseOrNombreContainingIgnoreCaseOrApellidoContainingIgnoreCase("PELL","PELL","PELL",new PageRequest(0,10));
+        Assert.assertThat(list.getContent(),hasSize(1));
+        Assert.assertThat(list.getContent().get(0).toString(),is(v.toString()));
+
+
     }
 
 
