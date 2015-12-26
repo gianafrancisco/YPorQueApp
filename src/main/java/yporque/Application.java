0a43e2ead57ebb4dc752545a6fec0eea032475cb
unknown <fgiana@PC-FGIANA-01.ARRS.ARRISI.com>
2015-12-26 11:16:40 -0300

    add defaults Articulos and Vendedores on startup

--- src/main/java/yporque/Application.java
+++ src/main/java/yporque/Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import yporque.model.Articulo;
 import yporque.model.Vendedor;
+import yporque.repository.ArticuloRepository;
 import yporque.repository.VendedorRepository;
 
 import java.util.List;
@@ -22,12 +24,25 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner loadData(VendedorRepository vendedorRepository) {
+	public CommandLineRunner loadData(VendedorRepository vendedorRepository, ArticuloRepository articuloRepository) {
 		return (args) -> {
-			List<Vendedor> list = vendedorRepository.findByUsername("Administrador");
+			List<Vendedor> list = vendedorRepository.findAll();
 			if(list.isEmpty()){
 				vendedorRepository.save(new Vendedor("Administrador","1qaz2wsx","Administrador","Administrador"));
+				vendedorRepository.save(new Vendedor("vendedor1","123456","Vendedor","1"));
+				vendedorRepository.save(new Vendedor("vendedor2","123456","Vendedor","2"));
+				vendedorRepository.save(new Vendedor("vendedor3","123456","Vendedor","3"));
+				vendedorRepository.save(new Vendedor("vendedor4","123456","Vendedor","4"));
 			}
+
+			List<Articulo> articulos = articuloRepository.findAll();
+			if(articulos.isEmpty()){
+				articuloRepository.save(new Articulo("100001","Remera",300.0,1.0,1.5,50,50));
+				articuloRepository.save(new Articulo("100002","Pantalon",450.0,1.0,2.0,10,10));
+				articuloRepository.save(new Articulo("100003","Campera",500.0,2.0,2.0,5,5));
+				articuloRepository.save(new Articulo("100004","Zapatilla",350.0,1.0,1.6,7,7));
+			}
+
 
 		};
 	}
