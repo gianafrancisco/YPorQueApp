b495a05c30eca26a9b7bac7ce2349f331bf66169
unknown <fgiana@PC-FGIANA-01.ARRS.ARRISI.com>
2016-01-11 21:11:01 -0300

    fixed some issues with Nota de Credito

--- src/main/resources/static/js/venderApp.js
+++ src/main/resources/static/js/venderApp.js
@@ -199,22 +199,34 @@ function venderController($scope,$http,$window,$location,$rootScope) {
    };
 
    $scope.notaCredito = function(){
-        var monto = new Number($scope.montoTotal);
-        if(monto < 0){
-            monto = -1*monto;
-            var notaCredito = {
-                articuloId: 6,
-                codigo: "N.CREDITO",
-                descripcion: "Nota de credito por la diferencia",
-                factor1: 1,
-                factor2: 1,
-                precioLista: monto,
-                precio: monto,
-                cantidadStock: 1,
-                cantidad: 1
-            };
-            $scope.agregarCarrito(notaCredito);
-        }
+
+        var search = "";
+        url = "/articulo/search?page=0&size=100&search=N.CREDITO";
+
+        $http.get(url)
+        .success(function(data, status, headers, config) {
+            var monto = new Number($scope.montoTotal);
+            if(monto < 0){
+                monto = -1*monto;
+                var notaCredito = data.content[0];
+                notaCredito.precioLista = monto;
+                notaCredito.precio = monto;
+                /*
+                var notaCredito = {
+                    articuloId: 6,
+                    codigo: "N.CREDITO",
+                    descripcion: "Nota de credito por la diferencia",
+                    factor1: 1,
+                    factor2: 1,
+                    precioLista: monto,
+                    precio: monto,
+                    cantidadStock: 1,
+                    cantidad: 1
+                };
+                */
+                $scope.agregarCarrito(notaCredito);
+            }
+        });
    };
 
    $scope.init = function(){
