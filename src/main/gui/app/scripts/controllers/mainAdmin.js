f55b09cbfdb7839a19e779c788271b2256eba9b8
Francisco Giana <gianafrancisco@gmail.com>
2016-11-23 09:11:59 -0300

    renamed files.

---
+++ src/main/gui/app/scripts/controllers/mainAdmin.js
@@ -0,0 +1,27 @@
+/*
+var app = angular.module('mainAdminApp', ['ngRoute','ui.bootstrap']).
+    config(function($routeProvider, $httpProvider) {
+
+             $routeProvider.
+               when('/articulo', {
+                 templateUrl: 'views/admin/articulo.html',
+                 controller: 'ArticuloController'
+               }).
+              when('/vendedor', {
+                templateUrl: 'views/admin/vendedor.html',
+                controller: 'VendedorController'
+              }).
+              when('/venta', {
+                templateUrl: 'views/admin/venta.html',
+                controller: 'VentaController'
+              })
+            .otherwise({
+                 redirectTo: '/articulo'
+               });
+           $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+           }
+)
+.controller('VentaController',ventaController)
+.controller('VendedorController',vendedorController)
+.controller('ArticuloController',articuloController);
+*/
